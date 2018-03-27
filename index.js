675432f978eb944f56a35f0c315c4c4997b2ca59
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-03-27 02:03:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(line){
   if(line.length > 0){
   //then say the line is empty
   } else {
-  // var message = 'The line is currently:
+  // var message = 'The line is currently:'
   //iterate through the line and add to a message # and name `${}`
   return 'The line is currently: 1. Bill, 2. Jane, 3. Ann';
   
