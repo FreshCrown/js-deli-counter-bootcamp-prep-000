9e873b4868dd1f7828013123701fadec332520c8
jsngomes@yahoo.com <jsngomes@yahoo.com>
2018-03-27 02:01:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ function currentLine(line){
   //if no one is in line
   //then say the line is empty
   //else
-  //iterate through the line and add to a message
+  // var message = The line is currently: 
+  //iterate through the line and add to a message # and name
   return 'The line is currently: 1. Bill, 2. Jane, 3. Ann';
   
 }
